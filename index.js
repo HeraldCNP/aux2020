f63ffb70498ab17573d4ab95d331fc5cc5befb99
Herald Choque <heraldcnp@gmail.com>
2021-02-21 20:17:59 -0400

    server creado 2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('funciona');
+})
+
+ server.listen(4000, () => {
+     console.log("server runnig port 4000");
+ })
